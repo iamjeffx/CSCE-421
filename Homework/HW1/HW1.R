72b42dd235b50ea1f63b2f9c4a60073f271e32a4
Jeffrey Xu <iamjeffx0@gmail.com>
2020-09-07 12:06:06 -0500

    Question 3 updated to include distance between the two points

--- Homework/HW1/HW1.R
+++ Homework/HW1/HW1.R
@@ -1,4 +1,6 @@
-x = c(1, .4, .4, 0, 0)
-y = c(1, .8, .6, .2, 0)
+x = c(100, 100000, 1000000, 10000000)
+y = c(45.1754, 0.394591, 0.308902, 0.353557)
 
-plot(x, y, xlab="FPR", ylab="TPR", main="ROC")
+plot(x, y, xlab="Capacity Size(B)", ylab="Time(s)")
+plot(log(x), y, xlab="Log of Capacity(B)", ylab="Time(s)")
+plot(log(x), 1/y, xlab="Log of Capacity(B)", ylab="Inverse of Time(Hz)")
